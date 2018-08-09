e2e32be50d3bac94202bf513ae7c3306feba4672
Leo Casagrande <leo@greenberry.io>
2018-08-09 15:55:23 +0200

    correction tp pokemon

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+// helpers
+
+function showPokemon(event) {
+    // console.log(event.target.textContent);
+    console.log(event.currentTarget);
+    window.location.href = './card.html?cardId=' + event.currentTarget.id;
+}
+
+
+
 // Ici, la requête sera émise de façon synchrone.
 const req = new XMLHttpRequest();
 req.open('GET', 'https://api.pokemontcg.io/v1/cards', false);
@@ -23,47 +33,90 @@ if (req.status === 200) {
 
 var pokeListe = document.createElement("ul");
 
-pokeListe.setAttribute("class",'pokeListe');
+pokeListe.setAttribute("class", 'pokeListe list');
 
 pokedex.cards.forEach(function(pokemon) {
-	console.log(pokemon);
-	var li = document.createElement("li");
-	li.setAttribute("class", "pokemon");
-	// insère le nom du pokemon dans un span
-	var spanNom = document.createElement("span");
-	spanNom.innerHTML = pokemon.name;
-	spanNom.setAttribute("class", "pokeNom")
-	li.appendChild(spanNom);
-	
-	// insérer l'image du pokemon dans la puce
+    console.log(pokemon);
+    var li = document.createElement("li");
+    li.setAttribute("class", "pokemon");
+    li.setAttribute("id", pokemon.id);
+    // insère le nom du pokemon dans un span
+    var spanNom = document.createElement("span");
+    spanNom.innerHTML = pokemon.name;
+    spanNom.setAttribute("class", "pokeNom")
+    li.appendChild(spanNom);
 
-	var img = document.createElement("img");
-	img.setAttribute("src",pokemon.imageUrl);
-	img.setAttribute("class","pokeImg");
-	li.appendChild(img);
+    // insérer l'image du pokemon dans la puce
 
-	// type plus numero pokedex
+    var img = document.createElement("img");
+    img.setAttribute("src", pokemon.imageUrl);
+    img.setAttribute("class", "pokeImg");
+    li.appendChild(img);
 
-	var spanBottom = document.createElement("span");
-	spanBottom.setAttribute("class", "bottom");
-	var spanType = document.createElement("span"); 
-	spanType.setAttribute("class", "bottomType");
-	var spanNum = document.createElement("span");
-	spanNum.setAttribute("class", "bottomNum"); 
+    // type plus numero pokedex
 
-	if(pokemon.types && pokemon.types.length > 0){
-		spanType.innerHTML = pokemon.types[0];
-		spanBottom.appendChild(spanType);
+    var spanBottom = document.createElement("span");
+    spanBottom.setAttribute("class", "bottom");
+    var spanType = document.createElement("span");
+    spanType.setAttribute("class", "bottomType");
+    var spanNum = document.createElement("span");
+    spanNum.setAttribute("class", "bottomNum");
+    spanNum.innerHTML = "num: " + pokemon.number;
+    spanBottom.appendChild(spanNum);
 
-	}
-	spanNum.innerHTML = pokemon.number;
-	spanBottom.appendChild(spanNum);
-	li.appendChild(spanBottom);
+    if (pokemon.types && pokemon.types.length > 0) {
+        spanType.innerHTML = "type: " + pokemon.types[0];
+        spanBottom.appendChild(spanType);
 
-	pokeListe.appendChild(li);
+
+    }
+
+
+    li.appendChild(spanBottom);
+
+    li.addEventListener("click", showPokemon, false);
+    pokeListe.appendChild(li);
 
 
 });
 
 var container = document.getElementById("pokeContainer");
 container.appendChild(pokeListe);
+var pagination = document.createElement("ul");
+pagination.setAttribute("class", "pagination");
+container.appendChild(pagination);
+// pagination avec listjs
+var monkeyList = new List('pokeContainer', {
+    valueNames: ['pokeNom'],
+    page: 12,
+    pagination: true
+});
+
+
+// Exemple d'itération dans un tableau pour remonter une valeur/objet particulière
+// exemple : je cherche l'id ex8-98 dans le tableau des pokemons
+
+// function rechercheIndex(table, id) {
+// 	// var toReturn;
+//     table.forEach(function(pokemon) {
+
+//         console.log(pokemon.id);
+
+//         if(pokemon.id === id){
+//         	console.log("pokemon trouvé, il s'agit de %s ", pokemon.name)
+//         	// console.log(pokemon);
+//  			  toReturn = pokemon;
+//         }
+//         else{
+//         	console.log("mauvais pokemon");
+//         }
+//     });
+
+//     return toReturn;
+// }
+
+// var monpokemon  = rechercheIndex(pokedex.cards, "ex8-98");
+// console.log(monpokemon);
+
+
+
---
+++ js/card.js
@@ -0,0 +1,49 @@
+console.log("je suis dans le profil");
+
+// Etape I = Récupération de l'id
+// passer l'url de la page dans une variable
+var url = window.location.href;
+
+// je récupère les search params
+var params = (new URL(document.location)).searchParams;
+
+// je récupère juste la clé qu'il me faut
+var cardId = params.get("cardId"); 
+
+console.log("ma carte est la carte %s",cardId);
+
+// Etape II. Remonter la carte correspondante - Interroger l'API,la db, etc)
+
+// Ici, la requête sera émise de façon synchrone.
+const req = new XMLHttpRequest();
+var urlToGet = 'https://api.pokemontcg.io/v1/cards/' + cardId;
+// alert(urlToGet);
+req.open('GET', urlToGet , false);
+
+req.send(null);
+
+if (req.status === 200) {
+    // console.log("Réponse reçue: %s", req.responseText --> string);
+    // console.log(req);
+    // console.log(typeof req.responseText);
+    // transformer une chaine en json 
+    var response = JSON.parse(req.responseText);
+    var monPokemon = response.card;
+
+    console.log(monPokemon);
+
+} else {
+    console.log("Status de la réponse: %d (%s)", req.status, req.statusText);
+}
+
+var cont = document.getElementById("container");
+
+var titre = document.createElement("h1");
+titre.innerHTML = monPokemon.name;
+var img = document.createElement("img");
+img.src= monPokemon.imageUrlHiRes;
+
+cont.appendChild(titre);
+cont.appendChild(img);
+
+
